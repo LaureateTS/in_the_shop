7a7f715043c6a140357611f76bda51989bcf50b4
Tristan Geraets <tristang@protonmail.ch>
2015-07-21 14:01:02 +0200

    add fixtures for some sample data

---
+++ server/fixtures.js
@@ -0,0 +1,81 @@
+if (People.find().count() === 0) {
+  // Create sample people
+  People.insert({
+    firstName: "Charles",
+    lastName: "Babbage",
+    homePhone: "0600000001",
+    mobilePhone: "0600000002"
+  });
+  People.insert({
+    firstName: "Alexander",
+    lastName: "Bell",
+    homePhone: "0600000003",
+    mobilePhone: "0600000004"
+  });
+  People.insert({
+    firstName: "Louis",
+    lastName: "Braille",
+    homePhone: "0600000005",
+    mobilePhone: "0600000006"
+  });
+  People.insert({
+    firstName: "Rudolf",
+    lastName: "Diesel",
+    homePhone: "0600000007",
+    mobilePhone: "0600000008"
+  });
+  People.insert({
+    firstName: "Thomas",
+    lastName: "Edison",
+    homePhone: "0600000009",
+    mobilePhone: "0600000010"
+  });
+  People.insert({
+    firstName: "Enrico",
+    lastName: "Fermi",
+    homePhone: "0600000011",
+    mobilePhone: "0600000012"
+  });
+  People.insert({
+    firstName: "Benjamin",
+    lastName: "Franklin",
+    homePhone: "0600000013",
+    mobilePhone: "0600000014"
+  });
+  People.insert({
+    firstName: "Samuel",
+    lastName: "Morse",
+    homePhone: "0600000015",
+    mobilePhone: "0600000016"
+  });
+  People.insert({
+    firstName: "Isaac",
+    lastName: "Newton",
+    homePhone: "0600000017",
+    mobilePhone: "0600000018"
+  });
+  People.insert({
+    firstName: "Nikola",
+    lastName: "Tesla",
+    homePhone: "0600000019",
+    mobilePhone: "0600000020"
+  });
+  People.insert({
+    firstName: "Alan",
+    lastName: "Turing",
+    homePhone: "0600000021",
+    mobilePhone: "0600000022"
+  });
+  People.insert({
+    firstName: "Alessandro",
+    lastName: "Volta",
+    homePhone: "0600000023",
+    mobilePhone: "0600000024"
+  });
+  People.insert({
+    firstName: "James",
+    lastName: "Watt",
+    homePhone: "0600000025",
+    mobilePhone: "0600000026"
+  });
+}
