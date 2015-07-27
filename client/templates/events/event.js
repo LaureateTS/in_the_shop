a6a23e37fd9e26fce8cfcdd8452838e0f547e72c
laureatets <tristang@protonmail.com>
2015-07-27 08:03:39 +0000

    fixed event confirm toggle

--- client/templates/events/event.js
+++ client/templates/events/event.js
@@ -1,8 +1,25 @@
 Template.event.helpers({
     isPersonDetailsContext: function() {
-        if (Router.current().route.path(this).substring(0, 21) === "/people/personDetails") return true;
+        if (Iron.Location.get().path.substring(0, 21) === "/people/personDetails") return true;
     },
     isConfirmed: function() {
+        var personId = Iron.Location.get().path.substring(22);
+        if (this.confirms) {
+            var numConfirms = this.confirms.length;
+            for (var i = 0; i < numConfirms; i++) {
+                if (this.confirms[i].person === personId &&
+                    this.confirms[i].confirmed == true) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+});
+
+Template.event.events({
+    "click .toggleConfirm": function() {
+        var personId = Iron.Location.get().path.substring(22);
+        Meteor.call('eventConfirmToggle', this._id, personId);
     }
 });
