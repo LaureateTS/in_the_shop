84dd887ef63e8247f22739c43dc2ee51ab34b61e
laureatets <tristang@protonmail.com>
2015-07-28 12:51:27 +0000

    changing workspace

--- client/templates/people/person.js
+++ client/templates/people/person.js
@@ -1,5 +1,26 @@
 Template.person.events({
     "click .toggleInOut": function() {
         Meteor.call('toggleInOut', this._id, !this.isIn);
+    },
+    "click .toggleConfirm": function(event) {
+        var personId = $(event.target).find('[name=personId]').val();
+        console.log(event);
+        console.log(this);
+        Meteor.call('eventConfirmToggle', this._id, personId);
     }
+});
+Template.person.helpers({
+    eventsForDropdown: function() {
+        var personId = this._id;
+        var eventsWithPerson = Events.find();
+        eventsWithPerson.forEach(function(o) {
+            o.personId = personId;
+            o.confirmed = o.confirms.filter(function(o) {
+                return o.confirmed === true && o.person == personId;
+            }).length;
+            console.log(o);
+        });
+        console.log(eventsWithPerson);
+        return eventsWithPerson;
+    },
 });
