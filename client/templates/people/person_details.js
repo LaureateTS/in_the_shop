8420d96714b6813aebec2413eafffc552ecae826
Tristan Geraets <tristang@protonmail.com>
2015-07-29 08:56:19 +0200

    more calendar stuff

--- client/templates/people/person_details.js
+++ client/templates/people/person_details.js
@@ -1,43 +1,50 @@
 Template.personDetails.events({
-    "submit form": function(event) {
-        event.preventDefault();
-        var message = {
-            userId: Meteor.userId(),
-            personId: $(event.target).find('[name=personId]').val(),
-            messageText: $(event.target).find('[name=message]').val(),
-            messageType: $(event.target).find('[name=messageType]').val()
-        };
-        Meteor.call('messageAdd', message, function(error, result) {
-            if (error) {
-                alert(error.reason);
-            }
-        });
-        event.target.message.value = "";
-    }
+  "submit form": function(event) {
+    event.preventDefault();
+    var message = {
+      userId: Meteor.userId(),
+      personId: $(event.target).find('[name=personId]').val(),
+      messageText: $(event.target).find('[name=message]').val(),
+      messageType: $(event.target).find('[name=messageType]').val()
+    };
+    Meteor.call('messageAdd', message, function(error, result) {
+      if (error) {
+        alert(error.reason);
+      }
+    });
+    event.target.message.value = "";
+  }
 });
 
 Template.personDetails.helpers({
-    
+
 });
 
-Template.personDetails.rendered = function(){
-    var calendar = $('#calendar').fullCalendar({
-        dayClick: function(date, jsEvent, view) {
-            var newAppt = {};
-            newAppt.start = date;
-            newAppt.end = date;
-            newAppt.title = "new event";
-            newAppt.createdBy = Meteor.userId();
-            newAppt.forPerson = Iron.Location.get().path.substring(22);
-            Meteor.call('appointmentAdd', newAppt, function(error,result){
-                if (error) {
-                    alert(error.reason);
-                }
-            });
+Template.personDetails.rendered = function() {
+  var calendar = $('#calendar').fullCalendar({
+    dayClick: function(date, jsEvent, view) {
+      var newAppt = {};
+      newAppt.start = date;
+      newAppt.end = date;
+      newAppt.title = "new event";
+      newAppt.createdBy = Meteor.userId();
+      newAppt.forPerson = Iron.Location.get().path.substring(22);
+      Meteor.call('appointmentAdd', newAppt, function(error, result) {
+        if (error) {
+          alert(error.reason);
         }
-
-    });
+      });
+    },
+    events: function(start, end, callback) {
+      var appointments = Appointments.find({}).fetch();
+      console.log(appointments);
+      callback(appointments);
+    }
+  }).data().fullCalendar;
+  Deps.autorun(function(){
+    Appointments.find({}).fetch();
+    if (calendar) {
+      calendar.refetchEvents();
+    }
+  })
 };
-
-
-
--- server/publications.js
+++ server/publications.js
@@ -33,5 +33,14 @@ Meteor.publish("getUserName", function(id) {
 // TODO only return upcoming event with default filter.
 // TODO add pagination for showing older events and to prevent anomalies when more upcoming events exist then the limit.
 Meteor.publish("events", function() {
-    return Events.find({},{sort: {eventDate:-1},limit:20});
+  return Events.find({}, {
+    sort: {
+      eventDate: -1
+    },
+    limit: 20
+  });
+});
+
+Meteor.publish("appointments", function(id) {
+  return Appointments.find({forPerson:id});
 });
